798743f85ba617ec66bdb033bc26c6a68a16da7a
Brandon Dias <dias.desenvolvimento@gmail.com>
2025-10-02 15:03:27 -0300

    Desbloqueio de niveis por arquivo de parametros

--- config/GameState.gd
+++ config/GameState.gd
@@ -1,5 +1,5 @@
 extends Node
 
 var board_setup = [
-	{"nivel": 1, "ordem":[],}, 
+	{"nivel": 1, "ordem":[1,2,3,4],}, 
 ]
--- scripts/Niveis.gd
+++ scripts/Niveis.gd
@@ -6,13 +6,24 @@ func set_main(main):
 	main_ref = main
 
 func _ready():
+	_set_niveis()
 	_conecta_botoes_de_nveis()
+	
+func _set_niveis():
+	for child in get_children():
+		if child is Button and "BotaoNivel" in child.name:
+			for board in GameState.board_setup:
+				if str(child.text) != str(board["nivel"]): 
+					child.disabled = true
+				else:
+					push_warning("Nivel Conquistado")
+		else:
+			push_warning("Child não é nível: ", child)
 	
 func _conecta_botoes_de_nveis():
 	for child in get_children():
 		if child is Button and "BotaoNivel" in child.name:
 			child.clicked.connect(_on_botao_nivel_clicado)
-			print("Conectando Botão: ", child)
 		else:
 			push_warning("Não conectado: ", child)
 
