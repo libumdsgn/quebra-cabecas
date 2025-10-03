eae14e32798b76dbbd5cfa78b6d045eccbb7f574
Brandon Dias <dias.desenvolvimento@gmail.com>
2025-10-03 11:49:26 -0300

    novos boards

--- config/GameParams.gd
+++ config/GameParams.gd
@@ -3,6 +3,8 @@ extends Node
 var tipo_boards = {
 	"DEFAULT": "res://boards/cells_2x2.tscn",
 	"3X2": "res://boards/cells_3x2.tscn",
+	"3X4": "res://boards/cells_3x4.tscn",
+	"4X5": "res://boards/cells_4x5.tscn",
 }
 
 var nivel_config ={
@@ -11,5 +13,18 @@ var nivel_config ={
 	3: tipo_boards["3X2"],
 	4: tipo_boards["3X2"],
 	5: tipo_boards["3X2"],
-	6: tipo_boards["3X2"],
+	7: tipo_boards["3X2"],
+	8: tipo_boards["3X2"],
+	9: tipo_boards["3X2"],
+	10: tipo_boards["3X4"],
+	11: tipo_boards["3X4"],
+	12: tipo_boards["3X4"],
+	13: tipo_boards["3X4"],
+	14: tipo_boards["3X4"],
+	15: tipo_boards["4X5"],
+	16: tipo_boards["4X5"],
+	17: tipo_boards["4X5"],
+	18: tipo_boards["4X5"],
+	19: tipo_boards["4X5"],
+	20: tipo_boards["4X5"],
 }
